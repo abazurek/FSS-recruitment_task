3548a81b5bdcaa0782ccaceff07066fe364b5881
abazurek <skala2294@gmail.com>
2021-01-28 16:29:32 +0100

    map created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import React from 'react';
 
+import MapComponent from "./components/MapComponent";
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <MapComponent />
   );
 }
 
