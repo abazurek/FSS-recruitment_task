d61dc17b54ca511cdaa99013efc1f76b65e8e970
abazurek <skala2294@gmail.com>
2021-02-01 23:29:02 +0100

    change for each fetch to parallel fetch

--- src/App.js
+++ src/App.js
@@ -1,54 +1,51 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import MapComponent from "./components/MapComponent";
 
 const weatherAPI = "https://danepubliczne.imgw.pl/api/data/synop/";
 
-function getGeolocationApi(city){
-    return `http://api.positionstack.com/v1/forward?access_key=4f2583fd551ab5f9d92ece8c1844737b&query=${city.stacja}`
+function getGeolocationApi(city) {
+  return `http://api.positionstack.com/v1/forward?access_key=4f2583fd551ab5f9d92ece8c1844737b&query=${city.stacja}`;
 }
 
 function App() {
-  const [data, setData]=useState("");
+  const [data, setData] = useState("");
 
+  async function getAllApiInfo() {
+    const results = [];
 
-    async function getAllApiInfo(){
-      const results = []
+    let responseWeather = await fetch(weatherAPI);
+    let dataWeather = await responseWeather.json();
 
-      let responseWeather = await fetch(weatherAPI);
-      let dataWeather = await responseWeather.json();
-
-      async function result(elem){
-              const array=[];
-              let responseGeolocation = (await fetch(getGeolocationApi(elem)))
-              let dataGeolocation = await responseGeolocation.json()
-              array.push(dataGeolocation.data[0].latitude,dataGeolocation.data[0].longitude );
-              elem = {...elem, latitude: array[0], longitude: array[1]};
-              results.push(elem);
+    async function getAllGeolocation(dataWeather) {
+      try {
+        await Promise.all(
+          dataWeather.map((elem) =>
+            fetch(getGeolocationApi(elem))
+              .then((response) => response.json())
+              .then((resp) =>
+                results.push({
+                  ...elem,
+                  latitude: resp.data[0].latitude,
+                  longitude: resp.data[0].longitude,
+                })
+              )
+          )
+        );
         return results;
+      } catch (error) {
+        console.log(error);
       }
+    }
 
-        async function asyncForEach(array, callback) {
-            for (let index = 0; index < array.length; index++) {
-                await callback(array[index], index, array);
-            }
-        }
+    const responses = await getAllGeolocation(dataWeather);
+    setData(responses);
+  }
 
-      await asyncForEach(dataWeather, result);
-      setData(results)
-     }
+  useEffect(function () {
+    getAllApiInfo();
+  }, []);
 
-
-
-  useEffect(function (){
-      getAllApiInfo();
-  },[])
-
-
-  return (
-    <MapComponent info={data}/>
-  );
+  return <MapComponent info={data} />;
 }
-
-
 
 export default App;
